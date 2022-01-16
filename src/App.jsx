03015b8e418553e9f58c6bae88fc7b8f76333b83
Rinasham <rinashamau@gmail.com>
2022-01-16 22:42:45 +1100

    [fix] html

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import './App.css';
 
 export const App = () => {
-  return <h1>hello</h1>
+  return(
+  <div className="App">
+    <h1 className="title">Todo List</h1>
+    <section className="common-area">
+      <h2 className="add-title">ADD TODO</h2>
+      <input type="text" />
+    </section>
+    <section className="common-area">
+      <ul className="todo-list">
+        <li className="todo">
+          <span className="todo-task">Task 1</span>
+          <i className="far fa-trash-alt"></i>
+        </li>
+        <li className="todo">
+          <span className="todo-task">Task 2</span>
+          <i className="far fa-trash-alt"></i>
+        </li>
+      </ul>
+    </section>
+  </div>
+  )
 };
 
